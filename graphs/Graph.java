e0289c2eadfa07cae3ebb4fb769146bf0b70e7f4
Chris <66870348+ChrispyChris123@users.noreply.github.com>
2023-08-18 10:30:45 -0500

    Graph beginnings

---
+++ graphs/Graph.java
@@ -0,0 +1,10 @@
+import java.util.*;
+//using https://www.geeksforgeeks.org/implementing-generic-graph-in-java/#
+//implementation
+//this is largely not my own code - Chris
+public class Graph<T> {
+    
+    private Map<T, List<T>> map = new HashMap<>();
+
+    
+}
